ab25cc80c458cb8ec5e74246320d4122bf30d9dd
KJH <kjhg5852@naver.com>
2021-02-22 11:51:25 +0900

    Project init

---
+++ src/main/kotlin/com/github/hwjames/studyspringkotlin/StudySpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hwjames.studyspringkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/hwjames/studyspringkotlin/StudySpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hwjames.studyspringkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudySpringKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
